6c8d94f1e05417183cf4c7da99aa6a58b5318114
linckrafael <rafaelinck@hotmail.com>
2023-02-16 22:24:25 -0300

    Correção

--- SEMANA1/[M1S01] Ex 9 - Banco (Desafio)/Program.cs
+++ SEMANA1/[M1S01] Ex 9 - Banco (Desafio)/Program.cs
@@ -1,29 +1,34 @@
 ﻿// Exercicio 9 - Banco Desafio
-int resposta;
+string resposta;
+List<decimal> extrato = new List<decimal>();
 do{
 Console.WriteLine("Bem vindo ao Banco Nacional Coqueiros, por favor selecione uma opção para continuar");
 Console.WriteLine("1. Adicionar Transação");
 Console.WriteLine("2. Ver extrato");
 Console.WriteLine("3. Sair");
-resposta = int.Parse ( Console.ReadLine());
-}while(!(resposta == 1 || resposta == 2 || resposta == 3));
+resposta = Console.ReadLine();
+
 
 decimal valor;
 
-if(resposta > 0 && resposta <= 2)
-{
-    List<decimal> extrato = new List<decimal>();
-    Console.WriteLine("Quantas transações?");
-    int qtsTran = int.Parse(Console.ReadLine());
-    for(int i = 0; i<qtsTran; i++){
-        Console.WriteLine("Digite sua " + (i+1) + " transação: ");
-            valor = decimal.Parse(Console.ReadLine());
-            extrato.Add(valor);
-    }
-            decimal sum = extrato.Sum();
-            Console.WriteLine("Seu extrato é: " + sum);
-}
+    if(resposta == "1")
+    {
+        Console.WriteLine("Quantas transações?");
+        int qtsTran = int.Parse(Console.ReadLine());
+        for(int i = 0; i<qtsTran; i++){
+            Console.WriteLine("Digite sua " + (i+1) + " transação: ");
+                valor = decimal.Parse(Console.ReadLine());
+                extrato.Add(valor);
+        }
 
-if(resposta == 3){
-    Console.WriteLine("Saindo");
-}
+    }
+
+    else if(resposta == "2"){
+        decimal sum = extrato.Sum();
+        Console.WriteLine("Seu extrato é: " + sum);
+    }
+
+    else if(resposta == "3"){
+        Console.WriteLine("Saindo");
+    }
+}while(!(resposta == "3"));
